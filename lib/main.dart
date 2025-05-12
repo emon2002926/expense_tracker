5077ae946ee8349bd678f8875cbdcdf44e5addbd
emon <emon79605@gmail.com>
2025-05-13 04:15:11 +0600

    Splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:expense_tracker/screens/authentication/bloc/auth_bloc.dart';
 import 'package:expense_tracker/screens/authentication/bloc/auth_event.dart';
 import 'package:expense_tracker/screens/authentication/login_page/login_screen.dart';
 import 'package:expense_tracker/screens/authentication/registration_page/register_screen.dart';
+import 'package:expense_tracker/screens/splash/splash_screen.dart';
 import 'package:expense_tracker/simple_bloc_observer.dart';
 import 'package:expense_tracker/util/app_colors.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:expense_repositories/src/repository/auth/auth_repository.dart' ;
+import 'package:expense_repositories/src/repository/auth/auth_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,31 +19,32 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final dataSource = FirebaseAuthDataSource();
     final authRepository = AuthRepositoryImpl(dataSource: dataSource);
     final signInUseCase = SignInUseCase(authRepository);
     final signUpUseCase = SignUpUseCase(authRepository);
+
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            colorScheme: ColorScheme.light(
-              primary: CustomColors.primary,
-              secondary: CustomColors.secondary,
-              tertiary: CustomColors.tertiary,
-              outline: CustomColors.outline,
-            )
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          primary: CustomColors.primary,
+          secondary: CustomColors.secondary,
+          tertiary: CustomColors.tertiary,
+          outline: CustomColors.outline,
         ),
-        home: BlocProvider(
-          create: (context) => 
-              AuthBloc(signInUseCase: signInUseCase, signUpUseCase: signUpUseCase)..add(AppStarted()),
-          child: LoginScreen(),
-        )
+      ),
+      home: BlocProvider(
+        create: (context) => AuthBloc(
+          signInUseCase: signInUseCase,
+          signUpUseCase: signUpUseCase,
+        ),
+        child: const SplashScreenPage(), // Use SplashScreenPage here
+      ),
     );
   }
-
 }
---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:expense_tracker/screens/home/bloc/get_expense_bloc/get_expense_bloc.dart';
+import 'package:expense_tracker/screens/authentication/bloc/auth_bloc.dart';
+import 'package:expense_tracker/screens/authentication/bloc/auth_event.dart';
+import 'package:expense_tracker/screens/authentication/bloc/auth_state.dart';
+import 'package:expense_tracker/screens/authentication/login_page/login_screen.dart';
+import 'package:expense_tracker/screens/home/screen/home_screen.dart';
+import 'package:expense_repositories/src/repository/expense/expense_repository.dart';
+import 'package:expense_repositories/src/repository/auth/auth_repository.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  const SplashScreenPage({super.key});
+
+  @override
+  State<SplashScreenPage> createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Trigger the AppStarted event after 10 seconds delay
+    Future.delayed(const Duration(seconds: 5), () {
+      context.read<AuthBloc>().add(AppStarted());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final dataSource = FirebaseAuthDataSource();
+    final authRepository = AuthRepositoryImpl(dataSource: dataSource);
+    final signInUseCase = SignInUseCase(authRepository);
+    final signUpUseCase = SignUpUseCase(authRepository);
+
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is AuthSuccess) {
+          // If user is authenticated, navigate to Home Screen
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (_) => BlocProvider(
+                create: (_) =>
+                GetExpenseBloc(FirebaseExpenseRepo())..add(GetExpense()),
+                child: const HomeScreen(),
+              ),
+            ),
+          );
+        } else if (state is AuthFailure || state is AuthInitial) {
+          // If user is not authenticated, navigate to Login Screen
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (_) => BlocProvider(
+                create: (context) =>
+                AuthBloc(signInUseCase: signInUseCase, signUpUseCase: signUpUseCase)
+                  ..add(AppStarted()),
+                child:  LoginScreen(),
+              ),
+            ),
+          );
+        }
+        // Keep splash screen visible while auth loading state is active
+      },
+      child:  Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Lottie.asset(
+            'assets/animations/wallet.json',
+            width: 200,
+            height: 200,
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
+    );
+  }
+}
